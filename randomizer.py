635c381fac6d85981cc0a70a91af3eb6550c28ba
weston-bailey <wdbailey87@gmail.com>
2022-04-29 17:42:38 -0700

    updates documentation

--- randomizer.py
+++ randomizer.py
@@ -10,32 +10,47 @@ args = sys.argv[1:]
 file = open(os.path.dirname(__file__) + "/students.json", "r")
 students_json = json.load(file)
 
-
 def get_next():
+    '''
+        prints the new student and advances the random list. if the random list is empty, it will be populated
+    '''
+    # create a random list if not present
     if len(students_json['random']) <= 0:
         make_rand()
 
+    # display whose turn it is
     print(f'{students_json["random"][0].replace("-", " ")} is up now.')
     
+    # inform the next student/that the list is empty
     if len(students_json['random']) <= 1:
         print("A new random list will be made next time!")
     else:
         print(f'{students_json["random"][1].replace("-", " ")} is up next time.')
+
+    # remove the person whose turn it is
     del students_json['random'][0]
 
+    # write the new list 
     with open(os.path.dirname(__file__) + "/students.json", "w") as write_file:
         json.dump(students_json, write_file)
+
 def print_rand():
+    '''
+        print out the students in order
+    '''
     for student in students_json['rand']:
         print(student)
 
 def make_rand():
-
+    '''
+        generates a new random list of students
+    '''
     # new list in random order
     rand_students = []
     # preserve the list of students 
     curr_students = students_json['students'].copy()
     
+    # create a random index, remove from curr_students and add to rand_students
     while len(curr_students) > 0:
         rand_index = randrange(0, len(curr_students), 1)
         rand_students.append(curr_students[rand_index])

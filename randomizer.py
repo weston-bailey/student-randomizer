a37db41f93c93f60fe932e6677f79a40946c4f6f
weston-bailey <wdbailey87@gmail.com>
2022-06-13 17:06:36 -0700

    pythonic-ify

--- randomizer.py
+++ randomizer.py
@@ -3,77 +3,95 @@ import sys
 import os
 from random import randrange
 
-# script args
-args = sys.argv[1:]
+class Randomizer:
+    def __init__(self):
+        self.students_json = self.__get_students_json()
+        self.args = sys.argv[1:]
 
-# open students file and pull data out as needed
-students_json = {}
-with open(os.path.dirname(__file__) + "/students.json", "r") as file:
-    students_json = json.load(file)
+    def __get_students_json(self):
+        '''
+            returns the studnets json from file
+        '''
+        # open students file and pull data out as needed
+        students_json = {}
+        with open(os.path.dirname(__file__) + "/students.json", "r") as file:
+            students_json = json.load(file)
+        return students_json
 
-def make_rand():
-    '''
-        generates a new random list of students
-    '''
-    # new list in random order
-    rand_students = []
-    # preserve the list of students 
-    curr_students = students_json['students'].copy()
+    def __write_students_json(self):
+        '''
+            writes the students json to file
+        '''
+        with open(os.path.dirname(__file__) + "/students.json", "w") as write_file:
+            json.dump(self.students_json, write_file)
 
-    # create a random index, remove from curr_students and add to rand_students
-    while len(curr_students) > 0:
-        rand_index = randrange(0, len(curr_students), 1)
-        rand_students.append(curr_students[rand_index])
-        del curr_students[rand_index]
+    def make_rand(self):
+        '''
+            generates a new random list of students
+        '''
+        # new list in random order
+        rand_students = []
+        # preserve the list of students 
+        curr_students = self.students_json['students'].copy()
 
-    # write file with new data
-    students_json['random'] = rand_students
-    with open(os.path.dirname(__file__) + "/students.json", "w") as write_file:
-        json.dump(students_json, write_file)
+        # create a random index, remove from curr_students and add to rand_students
+        while len(curr_students) > 0:
+            rand_index = randrange(0, len(curr_students), 1)
+            rand_students.append(curr_students[rand_index])
+            del curr_students[rand_index]
 
-def get_next():
-    '''
-        prints the new student and advances the random list. if the random list is empty, it will be populated
-    '''
-    # create a random list if not present
-    if len(students_json['random']) <= 0:
-        make_rand()
+        # write file with new data
+        self.students_json['random'] = rand_students
+        self.__write_students_json()
 
-    # display whose turn it is
-    print(f'{students_json["random"][0].replace("-", " ")} is up now.')
+    def get_next(self):
+        '''
+            prints the new student and advances the random list. if the random list is empty, it will be populated
+        '''
+        # create a random list if not present
+        if len(self.students_json['random']) <= 0:
+            self.make_rand()
 
-    # inform the next student/that the list is empty
-    if len(students_json['random']) <= 1:
-        print("A new random list will be made next time!")
-    else:
-        print(f'{students_json["random"][1].replace("-", " ")} is up next time.')
+        # display whose turn it is
+        print(f'{self.students_json["random"][0].replace("-", " ")} is up now.')
 
-    # remove the person whose turn it is
-    del students_json['random'][0]
+        # inform the next student/that the list is empty
+        if len(self.students_json['random']) <= 1:
+            print("A new random list will be made next time!")
+        else:
+            print(f'{self.students_json["random"][1].replace("-", " ")} is up next time.')
 
-    # write the new list 
-    with open(os.path.dirname(__file__) + "/students.json", "w") as write_file:
-        json.dump(students_json, write_file)
+        # remove the person whose turn it is
+        del self.students_json['random'][0]
 
-def print_rand():
-    '''
-        print out the students in order
-    '''
-    for student in students_json['rand']:
-        print(student)
+        # write the new list  
+        self.__write_students_json()
 
-def list_upcoming():
-    print("upcoming students:")
-    for i, student in enumerate(students_json["random"]):
-        print(f'{i + 1} {student}')
+    def print_rand(self):
+        '''
+            print out the students in order
+        '''
+        for student in self.students_json['rand']:
+            print(student)
 
-switch = {
-    '--next': get_next,
-    '--make': make_rand,
-    '--print': print_rand,
-    '--list': list_upcoming
-}
+    def list_upcoming(self):
+        print("upcoming students:")
+        for i, student in enumerate(self.students_json["random"]):
+            print(f'{i + 1} {student}')
 
-for arg in args:
-    if arg in switch:
-        switch[arg]()
+    def evaluate_args(self):
+        switch = {
+            '--next': self.get_next,
+            '--make': self.make_rand,
+            '--print': self.print_rand,
+            '--list': self.list_upcoming
+        }
+
+        for arg in self.args:
+            if arg in switch:
+                switch[arg]()
+
+        return self
+
+randomizer = Randomizer()
+randomizer.evaluate_args()
